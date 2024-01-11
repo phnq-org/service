a65e9a8ebd8a82a28ff4b20e787fb5f1f3ee7919
Patrick Gostovic <pg@phranq.com>
2024-01-11 11:10:05 +0100

    Comment

--- src/api/ApiMessage.ts
+++ src/api/ApiMessage.ts
@@ -12,6 +12,12 @@ export interface ApiResponseMessage extends ApiMessage {
 }
 
 export interface ApiNotificationMessage extends ApiMessage {
+  /**
+   * For now, this is the id of the WebSocket connection to send the notification to.
+   * This attribute can be generalized to support other types of recipients, like:
+   * - identity: the user's identity (i.e. via authentication)
+   * - subscription: the key for a subscription (i.e. via pub/sub)
+   */
   recipient: { id: string };
 }
 
