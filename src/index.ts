951dbcafe101ed3db7c4509a0317a536d6cfcc25
Patrick Gostovic <Patrick.Gostovic@wmg.com>
2025-09-21 12:51:57 -0400

    Exposed createContextFactory

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import ApiService from "./api/ApiService";
 import AuthService from "./auth/AuthService";
-import Context, { type Serializable } from "./Context";
+import Context, { createContextFactory, type Serializable } from "./Context";
 import Service, { type Handler } from "./Service";
 import ServiceClient from "./ServiceClient";
 
@@ -8,6 +8,7 @@ export {
   ApiService,
   AuthService,
   Context,
+  createContextFactory,
   Service,
   ServiceClient,
   type Serializable,
