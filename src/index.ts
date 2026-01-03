164693ff06e015a7ef934c5b80717f4d0e3fea36
Patrick Gostovic <Patrick.Gostovic@wmg.com>
2026-01-03 18:25:41 -0500

    Client stuff

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import ApiClient from "./api/ApiClient";
 import ApiService from "./api/ApiService";
+import type { AuthResult } from "./auth/AuthApi";
 import AuthService from "./auth/AuthService";
 import Context from "./Context";
 import Service, { type Handler } from "./Service";
@@ -6,4 +8,13 @@ import ServiceClient from "./ServiceClient";
 
 export * from "./Context";
 
-export { ApiService, AuthService, Context, Service, ServiceClient, type Handler };
+export {
+  ApiClient,
+  ApiService,
+  AuthService,
+  Context,
+  Service,
+  ServiceClient,
+  type Handler,
+  type AuthResult,
+};
